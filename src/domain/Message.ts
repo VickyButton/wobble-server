94cbde1c13b6bea7f5dfbcd220aa5b2400987915
Vicky <vickythewebdev@gmail.com>
2026-03-06 21:20:19 -0600

    [Feature] Create MessageService

--- src/domain/Message.ts
+++ src/domain/Message.ts
@@ -1,6 +1,6 @@
 // A message belongs to a user and a room
 export interface Message {
-  parentId?: string; // The ID of the message being replied to, if a reply
+  parentId?: string; // The ID of the message being replied to (optional)
   userId: string; // The ID of the user which the message was published by
   roomId: string; // The ID of the room which the message was posted in
   id: string;
---
+++ src/services/MessageService.ts
@@ -0,0 +1,105 @@
+import type { DateTimeProvider } from '../providers/DateTimeProvider';
+import type { IdProvider } from '../providers/IdProvider';
+import type { MessageRepository } from '../repositories/MessageRepository';
+
+export const messageErrors = {
+  UNABLE_TO_GET_MESSAGE: 'UNABLE_TO_GET_MESSAGE',
+  UNABLE_TO_CREATE_MESSAGE: 'UNABLE_TO_CREATE_MESSAGE',
+  UNABLE_TO_UPDATE_MESSAGE: 'UNABLE_TO_UPDATE_MESSAGE',
+  UNABLE_TO_DELETE_MESSAGE: 'UNABLE_TO_DELETE_MESSAGE',
+};
+
+export class MessageService {
+  private readonly idProvider: IdProvider;
+  private readonly dateTimeProvider: DateTimeProvider;
+  private readonly messageRepository: MessageRepository;
+
+  constructor(idProvider: IdProvider, dateTimeProvider: DateTimeProvider, messageRepository: MessageRepository) {
+    this.idProvider = idProvider;
+    this.dateTimeProvider = dateTimeProvider;
+    this.messageRepository = messageRepository;
+  }
+
+  public async getMessagesByParentId(parentId: string) {
+    return await this.messageRepository.getMessagesByParentId(parentId);
+  }
+
+  public async getMessagesByUserId(userId: string) {
+    return await this.messageRepository.getMessagesByUserId(userId);
+  }
+
+  public async getMessagesByRoomId(roomId: string) {
+    return await this.messageRepository.getMessagesByRoomId(roomId);
+  }
+
+  public async getMessageById(id: string) {
+    return await this.messageRepository.getMessageById(id);
+  }
+
+  public async createMessage({ parentId, userId, roomId, body }: {
+    parentId?: string;
+    userId: string;
+    roomId: string;
+    body: string;
+  }) {
+    const id = this.idProvider.generateId();
+    const createdAt = this.dateTimeProvider.now();
+    const updatedAt = createdAt;
+
+    try {
+      return await this.messageRepository.createMessage({
+        parentId,
+        userId,
+        roomId,
+        id,
+        body,
+        createdAt,
+        updatedAt,
+      });
+    } catch {
+      throw new Error(messageErrors.UNABLE_TO_CREATE_MESSAGE);
+    }
+  }
+
+  public async updateMessage(id: string, { body }: {
+    body: string;
+  }) {
+    const message = await this.messageRepository.getMessageById(id);
+
+    if (!message) {
+      throw new Error(messageErrors.UNABLE_TO_GET_MESSAGE);
+    }
+
+    const updatedAt = this.dateTimeProvider.now();
+
+    try {
+      return await this.messageRepository.updateMessage({
+        parentId: message.parentId,
+        userId: message.userId,
+        roomId: message.roomId,
+        id,
+        body,
+        createdAt: message.createdAt,
+        updatedAt,
+      });
+    } catch {
+      throw new Error(messageErrors.UNABLE_TO_UPDATE_MESSAGE);
+    }
+  }
+
+  public async deleteMessagesByRoomId(roomId: string) {
+    try {
+      return await this.messageRepository.deleteMessagesByRoomId(roomId);
+    } catch {
+      throw new Error(messageErrors.UNABLE_TO_DELETE_MESSAGE);
+    }
+  }
+
+  public async deleteMessage(id: string) {
+    try {
+      return await this.messageRepository.deleteMessage(id);
+    } catch {
+      throw new Error(messageErrors.UNABLE_TO_DELETE_MESSAGE);
+    }
+  }
+}
